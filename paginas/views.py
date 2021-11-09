0f58278e2f688a8c6fb38468b541af03329bfeb2
Rafael Malaquias Azevedo <malaquiasazv@gmail.com>
2021-11-09 20:39:13 -0300

    Primeira apresentação

--- paginas/views.py
+++ paginas/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 class PaginaInicial(TemplateView):
     template_name = 'paginas/index.html'
+
 
 class Sobre(TemplateView):
     template_name = 'paginas/sobre.html'
