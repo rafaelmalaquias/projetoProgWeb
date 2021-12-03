104085d0bc3fb07a7acfd88bdea5c364a609e700
Rafael Malaquias Azevedo <malaquiasazv@gmail.com>
2021-12-03 12:15:53 -0300

    Criação dos models, views e urls da Classe Nutriente

--- Blog/settings.py
+++ Blog/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'paginas.apps.PaginasConfig',
+    'cadastros.apps.CadastrosConfig',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
